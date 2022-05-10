e7bd0ea093ca67070b137002d11ca75f3a692ae0
EC2 Default User <ec2-user@ip-172-31-94-249.ec2.internal>
2022-05-10 10:10:39 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,28 +7,48 @@ class BooksController < ApplicationController
 
   def show
    @book = Book.find(params[:id])
-   @user = User.find(params[:id])
+   @user = @book.user
    @books = @user.books
    @profile_image = @user.profile_image
+   @newbook = Book.new
   end
 
   def edit
-    @book = Book.find(params[:id])
+  @book = Book.find(params[:id])
   end
 
   def create
-   flash[:notice] = "You have created book successfully."
    @book = Book.new(book_params)
    @book.user_id = current_user.id
-   @book.save
-   redirect_to books_path
+   if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+   else
+      @books = Book.all
+      @user = current_user
+      render :index
+
+   end
+   
   end
 
   def update
-    flash[:notice] = " You have updated book successfully."
-
+     @book = Book.find(params[:id])
+     if @book.update(book_params)
+      flash[:notice] = "You have update book successfully."
+      redirect_to book_path(@book.id)
+     else
+      @books = Book.all
+      @user = current_user
+      render :index
+     end
   end
 
+  def destroy
+  @book = Book.find(params[:id])
+  @book.destroy
+  redirect_to books_path
+  end
 
   private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
-   @users = User.all
-   @user = current_user
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+
   end
 
   def show
@@ -13,19 +15,21 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
-    flash[:notice] = " You have updated user successfully."
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+     if @user.update(user_params)
+      flash[:notice] = "You have update user successfully."
+      redirect_to user_path(@user.id)
+     else
+      @user = current_user
+      render :edit
+     end
   end
-
-  def create
-    flash[:notice] = " Welcome! You have signed up successfully."
-  end
-
 
   private
   def user_params
